b46da18ec976f67bdf52f08324d03d228c0d7ee0
Andrew Poydence <apoydence@gmail.com>
2015-10-20 06:34:40 -0600

    Adds example

---
+++ example/example.go
@@ -0,0 +1,28 @@
+package main
+
+import "github.com/apoydence/lightsoff"
+
+func main() {
+	someChannel := make(chan int, 10)
+	count := 5
+	lights := lightsoff.New(count, func() {
+		close(someChannel)
+	})
+
+	for i := 0; i < count; i++ {
+		go run(someChannel, lights)
+	}
+
+	for range someChannel {
+		//NOP
+	}
+}
+
+func run(someChannel chan<- int, lights *lightsoff.LightsOff) {
+	defer lights.TurnOff()
+	defer println("Done")
+
+	for i := 0; i < 100; i++ {
+		someChannel <- i
+	}
+}
